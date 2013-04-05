e731abff9441f4fb18064fb475c9b0fa36c27a62
A11339 <akihiro.kuwano@gmail.com>
2013-04-05 15:38:37 +0900

    3.3.0 対応.

--- 08/test.sh
+++ 08/test.sh
@@ -1,2 +1,2 @@
-export C_INCLUDE_PATH=/Users/A11339//.pyenv/versions/2.7.3/include/python2.7:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=/Users/A11339//.pyenv/versions/2.7.3/include/python2.7:$CPLUS_INCLUDE_PATH
+export C_INCLUDE_PATH=/Users/A11339//.pyenv/versions/3.3.0/include/python2.7:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/Users/A11339//.pyenv/versions/3.3.0/include/python2.7:$CPLUS_INCLUDE_PATH
