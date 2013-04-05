a823aad5ba70b4e8188578d4cd63f6e12df7ce75
A11339 <akihiro.kuwano@gmail.com>
2013-04-05 14:36:00 +0900

    setup.py added.

--- 08/dumb_print.c
+++ 08/dumb_print.c
@@ -1,28 +1,43 @@
+/* head fileのインクルード */
 #include <Python.h>
 
-# Python で dumb_print.print() すると起動される関数
+/* Python で dumb_print.print() すると起動される関数 */
 static PyObject *
 dumb_print(PyObject *self, PyObject *args) {
+        /* unicode文字列をUTF-iエンコーディングのchar文字列に変換する */
 	const char *text;
 	if (!PyArg_ParseTuple(args, "s", &text))
 		return NULL;
+        /* char文字列の出力 */
 	printf("%s\n", text);
+        /* 戻り値の指定：ここではなし */
 	Py_RETURN_NONE;
+        /* 読み替えると以下
+	   Py_INCREF(Py_Node);
+	   return Py_None;  */
 }
 
-static PyMethodDef dumb_print_medhods[] = {
-	{"print", dumb_print, METH_VARARGES, "Print text"},
+/* 拡張モジュールで提供する関数のリスト */
+static PyMethodDef dumb_print_methods[] = {
+	{"print", dumb_print, METH_VARARGS, "Print text"},
+        /* sentinel リストの終わりを示すものかな？ */
 	{NULL, NULL, 0, NULL}
 };
 
+/* 拡張モジュールのデータを登録する構造体:PyModuleDef */
 static struct PyModuleDef dumb_print_module = {
 	PyModuleDef_HEAD_INIT,
+        /* モジュール名 */
 	"dumb_print",
+        /* どきゅめんてーしょんもじれつ */
 	"sample extension module",
+        /* Pythonインタプリタで管理するモジュール固有データのサイズ。独自にグローバル変数で管理する場合は-1 */
 	-1,
-	dumb_print_medhods
+        /* PyMethodDefへのポインタ */
+	dumb_print_methods
 };
 
+/* ライブラリのロード＆初期化 PyInit_HOGEHOGE */
 PyMODINIT_FUNC
 PyInit_dumb_print(void) {
 	return PyModule_Create(&dumb_print_module);
---
+++ 08/test.sh
@@ -0,0 +1,2 @@
+export C_INCLUDE_PATH=/Users/A11339//.pyenv/versions/2.7.3/include/python2.7:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/Users/A11339//.pyenv/versions/2.7.3/include/python2.7:$CPLUS_INCLUDE_PATH
